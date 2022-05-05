4d37db86133cec6729c425b2c8d64de4093a8f55
Sloan <Sloan.boyce@gmail.com>
2022-05-05 12:53:57 -0400

    added file selection rather than folder button

--- yonde/src/components/FileSelect.js
+++ yonde/src/components/FileSelect.js
@@ -3,7 +3,7 @@ const FileSelected = () => {
         <div className="bg-gray-700 min-h-screen flex flex-col items-center justify-center p-0 m-0">
             <div className="text-gray-500 text-center text-3xl">
                 <p>Please select a root for your manga folder.</p>
-                <button className="text-white bg-gray-500 p-5 m-3 hover:cursor-pointer hover:shadow-xl rounded-md">Select Folder</button>
+                <input className="text-white bg-gray-500 p-5 m-3 hover:cursor-pointer hover:shadow-xl rounded-md" type="file" />
             </div>
         </div>
     )
