0be399d924753e1e0d23a2005899fabeb178120c
Sloan <Sloan.boyce@gmail.com>
2022-05-04 22:16:45 -0400

    added more responsiveness

--- yonde/src/components/FileSelect.js
+++ yonde/src/components/FileSelect.js
@@ -2,8 +2,8 @@ const FileSelected = () => {
     return (
         <div className="bg-gray-700 min-h-screen flex flex-col items-center justify-center p-0 m-0">
             <div className="text-gray-500 text-center text-3xl">
-                <p>Please a root for your manga folder.</p>
-                <button className="text-white bg-gray-500 p-5 m-3 hover:cursor-pointer">Select Folder</button>
+                <p>Please select a root for your manga folder.</p>
+                <button className="text-white bg-gray-500 p-5 m-3 hover:cursor-pointer hover:shadow-xl rounded-md">Select Folder</button>
             </div>
         </div>
     )
